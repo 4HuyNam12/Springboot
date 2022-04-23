03009774e5783adb0e7c7164cc0a121dc921e3da
4HuyNam12 <formysadsong412@gmail.com>
2022-04-23 15:53:01 +0700

    create Message exception class

---
+++ Less2/Job/src/main/java/vn/techmaster/job/exception/Message.java
@@ -0,0 +1,7 @@
+package vn.techmaster.job.exception;
+
+public class Message {
+    public static final String notFoundJobMessage = "Job not found";
+    public static final String jobWithID = "Job with ID: ";
+    public static final String listNoJob = "No Job, lets add";
+}
